6f4675b72aaa542d8cd03ab88cdeb19f3eb1cc45
Vladyslav <vladizar123@gmail.com>
2020-06-26 17:00:41 +0300

    Added slider and popup

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// POPUP
+
+// Find all needed DOM elements
+const popupFeedback = document.querySelector('.popup-feedback');
+const popupFeedbackOpenButton = document.querySelector('.write-us');
+const popupFeedbackCloseButton = popupFeedback.querySelector('.button-close');
+const popupFeedbackForm = popupFeedback.querySelector('form');
+const popupFeedbackFormInputs = popupFeedbackForm.querySelectorAll('input');
+const popupFeedbackFormTextarea = popupFeedbackForm.querySelector('textarea');
+
+// When user clicks on popup open button
+popupFeedbackOpenButton.onclick = function (evt) 
+{
+    // Stop browser from following the link
+    evt.preventDefault();
+
+    // Show popup
+    popupFeedback.classList.remove('hidden');
+}
+
+// When user clicks on popup close button
+popupFeedbackCloseButton.onclick = function () 
+{
+    // Hide popup
+    popupFeedback.classList.add('hidden');
+}
+
+// When user submits popup form
+popupFeedbackForm.onsubmit = function (evt) 
+{
+    // Iterate over inputs in form
+    for (const popupFeedbackFormInput of popupFeedbackFormInputs) 
+    {
+        // If any empty value
+        if (!popupFeedbackFormInput.value || !popupFeedbackFormTextarea.value) 
+        {
+            // Stop browser from sending the form
+            evt.preventDefault();
+            
+            // Show user error animation
+            popupFeedback.classList.remove('popup-error');
+            void popupFeedback.offsetWidth;
+            popupFeedback.classList.add('popup-error');
+
+            // Stop the loop
+            break;
+        }
+    }
+}
+
+// SLIDER
+
+// Find all needed DOM elements
+const sliderControls = document.querySelectorAll('.slider-control');
+const sliderPages = document.querySelectorAll('.slider-page');
+
+// Storage for current showing page number
+let currentPage = 0;
+
+// Iterate over slider controls
+for (let i = 0; i < sliderControls.length; i++) 
+{
+    // When user clicks on slider control
+    sliderControls[i].onclick = function () 
+    {
+        // Hide current active control activeness view
+        sliderControls[currentPage].classList.remove('active-control');
+
+        // Hide current showing slider page
+        sliderPages[currentPage].classList.add('hidden');
+
+        // Change current showing page number
+        currentPage = i;
+
+        // Show new active control
+        this.classList.add('active-control');
+
+        // Show new slider page
+        sliderPages[currentPage].classList.remove('hidden');
+    }
+}
